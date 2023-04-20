3dae32a1616577e7a0ddffe5b47ef94ae74928c1
giovanamacedo <giovanaml12@gmail.com>
2023-04-19 22:29:17 -0300

    operadores

---
+++ src/ModuloUm/Operadores/Main.java
@@ -0,0 +1,18 @@
+package ModuloUm.Operadores;
+
+public class Main {
+    public static void main(String[] args) {
+        int anoLancamento = 2005;
+        double notaDoFilme = 8.1;
+        boolean incluidoNoPlano = true;
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        String sinopse = "Filme de Ficção Animado";
+        int classificacao = (int) media / 2;
+
+        System.out.println("Esse é o Stream Match.");
+        System.out.println("Filme: Noiva Cadáver. Ano de lançamento: " + anoLancamento);
+        System.out.println(media);
+        System.out.println(sinopse);
+
+    }
+}
